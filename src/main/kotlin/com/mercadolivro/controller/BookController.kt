c3dd2ae5c2ff49229cd6cafb71c396a5fc617e45
Gian Haack <gianhaack@macos.local>
2021-08-18 09:01:35 -0300

    Commit inicial com os models de customer e books. Controlers, Services e Repositoy iniciais. Impl do flyway

---
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -0,0 +1,11 @@
+package com.mercadolivro.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("books")
+class BookController {
+    
+
+}
