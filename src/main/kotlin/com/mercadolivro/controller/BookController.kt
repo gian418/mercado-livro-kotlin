dfbfc23d0e8fcc16d3f1d22481fb9c185d60b37f
Giancarlo Haack Albano <gian418@gmail.com>
2021-08-18 11:17:20 -0300

    Adicionando endpoints gerais para book.

--- src/main/kotlin/com/mercadolivro/controller/BookController.kt
+++ src/main/kotlin/com/mercadolivro/controller/BookController.kt
@@ -2,6 +2,8 @@ package com.mercadolivro.controller
 
 import com.mercadolivro.controller.extension.toBookModel
 import com.mercadolivro.controller.request.PostBookRequest
+import com.mercadolivro.controller.request.PutBookRequest
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.service.BookService
 import com.mercadolivro.service.CustomerService
 import org.springframework.http.HttpStatus
@@ -19,6 +21,32 @@ class BookController(
     fun create(@RequestBody request: PostBookRequest) {
         val customer = customerService.getById(request.customerId)
         bookService.create(request.toBookModel(customer))
+    }
+
+    @GetMapping
+    fun findAll(): List<BookModel> {
+        return bookService.findAll()
+    }
+
+    @GetMapping("/actives")
+    fun findActive(): List<BookModel> = bookService.findActives()
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Int): BookModel {
+        return bookService.findById(id)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun delete(@PathVariable id: Int) {
+        bookService.delete(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: Int, @RequestBody book: PutBookRequest) {
+        var bookSaved = bookService.findById(id)
+        bookService.update(book.toBookModel(bookSaved))
     }
 
 }
