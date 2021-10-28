7cdafb3c66cdaa8974cba315c1d79609fab911dc
Giancarlo Haack Albano <gian418@gmail.com>
2021-10-28 10:54:12 -0300

    fixes

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -42,6 +42,10 @@ class SecurityConfig(
         "/admins/**"
     )
 
+    private val PUBLIC_GET_MATCHERS = arrayOf(
+        "/books"
+    )
+
     override fun configure(auth: AuthenticationManagerBuilder) {
         auth.userDetailsService(userDatails).passwordEncoder(bCryptPasswordEncoder())
     }
@@ -52,6 +56,7 @@ class SecurityConfig(
         http.authorizeRequests()
             .antMatchers(*PUBLIC_MATCHERS).permitAll()
             .antMatchers(HttpMethod.POST, *PUBLIC_POST_MATCHERS).permitAll()
+            .antMatchers(HttpMethod.GET, *PUBLIC_GET_MATCHERS).permitAll()
             .antMatchers(*ADMIN_MATCHERS).hasAuthority(Role.ADMIN.description)
             .anyRequest().authenticated()
 
